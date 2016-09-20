cccdceb40cc8229d5ce2f8eaac292652519fa118
Ritesh Agrawal <ragrawal@uber.com>
2016-09-20 10:18:08 -0700

    remove close button

--- js/flashcards.js
+++ js/flashcards.js
@@ -70,7 +70,7 @@ function populateCards(){
           // $("#cards").append('<div class="col-md-3"><a href="#" data-toggle="modal" data-target="#card' + secNo + '" class="thumbnail well card-thumbnail"><h3>' + heading + '</h3></a></div>');
         $("#cards").append('<li class="col-sm-4 col-md-3 col-lg-3"><a href="#" data-toggle="modal" data-target="#card' + secNo + '" class="thumbnail name well"><h4>' + heading + '</h4></a></li>');                                            
 
-        $("body").append('<div class="modal fade" id="card' + secNo + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">' + heading + '</h4></div><div class="modal-body"><div class="card-content">' + converter.makeHtml(body) + '</div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+        $("body").append('<div class="modal fade" id="card' + secNo + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">' + heading + '</h4></div><div class="modal-body"><div class="card-content">' + converter.makeHtml(body) + '</div></div><div class="modal-footer"></div></div></div></div>');
 
         numCards += 1;
 
