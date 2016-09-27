4c18d62b46f99f2b1eed760174e644478f418a3a
Ritesh Agrawal <ragrawal@uber.com>
2016-09-27 06:27:34 -0700

    fixed influence of javascript to other tabs

--- js/background.js
+++ js/background.js
@@ -1,3 +1,21 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(null, {'file': 'js/flashcards.js'});
+	// inject Javascript
+	var files = [
+          "js/jquery.js"
+          , "js/bootstrap.min.js"
+          , "js/bootstrap-select.min.js"
+          , "js/list.js"
+          , "js/list.fuzzysearch.js"
+          , "js/list.pagination.js"
+          , "vendor/showdown-1.4.2/dist/showdown.min.js" 
+          , "vendor/showdown-1.4.2/dist/showdown-prettify.min.js"
+          , "vendor/showdown-1.4.2/dist/showdown-tables.min.js"
+          , "js/flipclock.min.js"
+          , "js/flashcards.js"
+      ];
+
+    for(i=0; i<files.length; i+=1){
+    	chrome.tabs.executeScript(null, {'file':files[i]});
+    }
+
 });
