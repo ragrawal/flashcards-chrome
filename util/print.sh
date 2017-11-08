2bd801af3f473f1976cc27d26f15fb508d57210f
ragrawal <ragrawal@uber.com>
2017-11-08 10:02:15 -0800

    fixed printing script

--- util/print.sh
+++
@@ -1,2 +0,0 @@
-cat $1 | sed -e $'s/^# .*/pagenumbering\{gobble\}/g' | sed -e 's/pagenumbering/\\pagenumbering/g' | sed -e $'s/^\##/newpage\\\n##/g' | sed -e 's/newpage/\\newpage/g' | sed -e 's/## \(.*\)\\n/**\1**/g' | pandoc -V geometry:papersize="{5in,3in}" -V geometry:margin=0.15in -f markdown -o $2
-open $2
